1e53398e213c3ff25b09e640a7e00a04dd347822
omondi-mutuma <omondimutuma@gmail.com>
2024-10-25 11:20:28 +0300

    Add custom styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 "use client";
+import Image from "next/image";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div className="px-[20px] md:px-[100px] min-h-screen flex flex-col">
       <Header />
 
-      <main className="flex-grow p-8">
-        <h1 className="text-3xl font-bold mb-4">Welcome to My App</h1>
-        {/* Add your main content here */}
+      <main className="flex flex-col justify-center items-center">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <Image
+            src={"/mobile-circles-bg.png"}
+            alt=""
+            width={390}
+            height={565}
+          />
+        </div>
+        <Hero />
       </main>
 
       <footer className="p-4 text-center">
