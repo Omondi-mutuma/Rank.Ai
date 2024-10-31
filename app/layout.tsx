6c2a22f4b8c0246a75219f3e49b2ba7029d016d1
omondi-mutuma <omondimutuma@gmail.com>
2024-10-31 22:05:54 +0300

    Add website metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Rank AI" />
+        <meta
+          property="og:description"
+          content="Get boosted ranking insights from our AI."
+        />
+        <meta
+          property="og:image"
+          content="https://rankings-ai-36f4.vercel.app/preview-image.png"
+        />{" "}
+        {/* Full URL to the image */}
+        <meta
+          property="og:url"
+          content="https://rankings-ai-36f4.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Rank AI" />
+        <meta
+          name="twitter:description"
+          content="Get boosted ranking insights from our AI."
+        />
+        <meta
+          name="twitter:image"
+          content="https://rankings-ai-36f4.vercel.app/preview-image.jpg"
+        />
+      </head>
       <body
         className={`${inter.className} antialiased px-[20px] md:px-[80px] min-h-screen flex flex-col`}
       >
